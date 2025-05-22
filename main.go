dd466f4ee0ff5c1280e55f0e4ea7df15819094ce
sinore69 <saptarshidutta599@gmail.com>
2025-05-23 01:31:02 +0530

    web scraper + bubbletea mvp

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"fmt"
 	"io"
+	"net/url"
 	"os"
+	"strconv"
 	"sync"
 
 	"webscraper/functions"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 const outputFileName = "output.txt"
-
 
 var (
 	visited = make(map[string]bool)
@@ -18,7 +21,128 @@ var (
 	wg      sync.WaitGroup
 )
 
+type Model struct {
+	url         string
+	concurrency int
+	phase       int
+	depth       int
+	input       string
+	message     string
+}
+
+// Init is called when the program starts
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+// Update handles key events
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyBackspace:
+			if len(m.input) > 0 {
+				m.input = m.input[:len(m.input)-1]
+			}
+		case tea.KeyEnter:
+			if m.input == "" {
+				break
+			}
+			if m.phase == 0 {
+				if isValidURL(m.input) {
+					m.url = m.input
+					m.phase = m.phase + 1
+					m.input = ""
+					break
+				} else {
+					m.message = "Invalid URL"
+				}
+			}
+			if m.phase == 1 {
+				if m.input == "" {
+					break
+				}
+				num, err := strconv.Atoi(m.input)
+				if err != nil {
+					m.message = "Invalid number1"
+					break
+				}
+				if num <= 0 {
+					m.message = "Concurrency must be greater than 0"
+					break
+				}
+				m.concurrency = num
+				m.input = ""
+				m.phase = m.phase + 1
+			}
+			if m.phase == 2 {
+				if m.input == "" {
+					break
+				}
+				num, err := strconv.Atoi(m.input)
+				if err != nil {
+					m.message = fmt.Sprintf("%v", err)
+					break
+				}
+				if num <= 0 {
+					m.message = "Depth must be greater than 0"
+					break
+				}
+				m.depth = num
+				m.input = ""
+				m.phase = m.phase + 1
+			}
+			if m.phase == 3 {
+				outputFile, err := os.Create(outputFileName)
+				if err != nil {
+					fmt.Println("Failed to create output file:", err)
+					break
+				}
+				defer outputFile.Close()
+				wg.Add(1)
+				go scrape(m.url, m.depth, outputFile)
+
+				wg.Wait()
+				fmt.Println("Scraping completed. Output saved to", outputFileName)
+				os.Exit(0)
+			}
+		case tea.KeyRunes:
+			m.input += string(msg.Runes)
+		}
+	}
+
+	return m, nil
+}
+
+// View renders the UI
+func (m Model) View() string {
+	switch m.phase {
+	case 0:
+		return fmt.Sprintf("Enter URL: %s\n%s", m.input, m.message)
+	case 1:
+		return fmt.Sprintf("Enter Max Concurrency level: %s\n%s", m.input, m.message)
+	case 2:
+		return fmt.Sprintf("Enter Max Depth level: %s\n%s", m.input, m.message)
+	// case 3:
+	// 	return fmt.Sprintln("scrapping website")
+	default:
+		return fmt.Sprintf("%+v", m)
+	}
+}
+
+func isValidURL(s string) bool {
+	parsed, err := url.ParseRequestURI(s)
+	if err != nil {
+		return false
+	}
+	return parsed.Scheme != "" && parsed.Host != ""
+}
+
 func scrape(link string, depth int, writer io.Writer) {
+
 	defer wg.Done()
 
 	if depth <= 0 {
@@ -71,29 +195,11 @@ func scrape(link string, depth int, writer io.Writer) {
 	}
 }
 
-
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run main.go <url> [depth]")
-		return
+
+	p := tea.NewProgram(Model{})
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
 	}
-
-	startURL := os.Args[1]
-	depth := 3 
-	if len(os.Args) >= 3 {
-		fmt.Sscanf(os.Args[2], "%d", &depth)
-	}
-
-	outputFile, err := os.Create(outputFileName)
-	if err != nil {
-		fmt.Println("Failed to create output file:", err)
-		return
-	}
-	defer outputFile.Close()
-
-	wg.Add(1)
-	go scrape(startURL, depth, outputFile)
-
-	wg.Wait()
-	fmt.Println("Scraping completed. Output saved to", outputFileName)
 }
